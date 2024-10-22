c4e97e79ca2386fa06b1c402edefbe5c479cf0b3
SwiftCris <115347840+SwiftCris@users.noreply.github.com>
2024-10-22 15:18:32 +0000

    O

--- test.js
+++ test.js
@@ -3,38 +3,38 @@ var rate = document.getElementById("1rate");
 var btn = document.getElementById("btn");
 var idk = document.getElementById("idk");
 var idk2 = document.getElementById("idk2");
-// LORD FORGIVE ME 
 var skibidi2 = document.getElementById("skibidi2");
 
 btn.addEventListener("click", func);
 
 function func() {
-  if (rate.value > 10) {
-    alert("Can't be higher than 10");
-    return;
-  }
-  if (rate.value < 1) {
-    alert("Can't be lower than 1");
-    return;
-  }
-  if (rate.value === "") {
-    alert("Please Rate Skibidi Toilet");
-    return;
-  }
+    // Convert the input value to a number
+    var rateValue = parseFloat(rate.value);
 
-  var rating = rate.value;
-  rating = rating * 100 / 10;
+    // Validation checks
+    if (rateValue > 10) {
+        alert("Can't be higher than 10");
+        return;
+    }
+    if (rateValue < 1) {
+        alert("Can't be lower than 1");
+        return;
+    }
+    if (isNaN(rateValue)) { // Changed to check for NaN
+        alert("Please Rate Skibidi Toilet");
+        return;
+    }
 
-  if (idk.checked) {
-    rating += 0.5;
-  }
-  if (idk2.checked) {
-    rating += 0.5;
-  }
-  if (skibidi2.checked) {
-    rating += 100;
-  }
+    var rating = rateValue * 10; // Scale from 0 to 100
+    if (idk.checked) {
+        rating += 0.5;
+    }
+    if (idk2.checked) {
+        rating += 0.5;
+    }
+    if (skibidi2.checked) {
+        rating += 100;
+    }
 
-  alert("Your rating is " + (rating % 1000) + "/100%");
+    alert("Your rating is " + rating + "/100%");
 }
-
